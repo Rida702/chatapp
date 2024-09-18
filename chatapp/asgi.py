f2b20f2e6b132bda58b6b306a78bbe907dcf0a11
Rida702 <ai22c0rida@gmail.com>
2024-09-18 13:23:03 +0500

    Create Chat room

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -1,31 +1,13 @@
-"""
-ASGI config for chatapp project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import path
-from chat import consumers
 
-django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            [
-                path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
-            ]
-        )
-    ),
-})
-
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)
