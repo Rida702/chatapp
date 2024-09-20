df32b6dfc7fe74fda508a734d51afbe77fecd5a5
Rida Shabbir <107110619+Rida702@users.noreply.github.com>
2024-09-20 14:36:22 +0500

    Update routing.py

--- chat/routing.py
+++ chat/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi(), name='one2one_chat'),
-    path('ws/chat/group/<str:group_name>/', consumers.ChatConsumer.as_asgi(), name='group_chat'),
+    path('ws/chat/<str:group_name>/', consumers.ChatConsumer.as_asgi(), name='group_chat'),
 ]
