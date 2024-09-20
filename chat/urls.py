dc6e8290ad36557eb737849f00c8442f045f623f
Rida702 <ai22c0rida@gmail.com>
2024-09-20 13:54:43 +0500

    Implement API development changes

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+"""
 urlpatterns = [
     path("select_user/", views.select_user, name="select_user"),
-    path("chat/room/<str:room_name>/", views.room, name="room"),
-    #path("chat/group/", views.create_group, name="create_group"),  
+    path("chat/room/<str:room_name>/", views.room, name="room"), 
+]
+"""
+
+urlpatterns = [
+    path('rooms/', views.RoomListCreateView.as_view(), name='room-list-create'),
 ]
